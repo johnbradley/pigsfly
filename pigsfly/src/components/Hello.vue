5e2a41df72309a1b0f4893b5ac319365b2d8106b
John Bradley <johnbradley2008@gmail.com>
2017-07-20 15:15:34 -0400

    ipython notebook and more interactive ui

--- pigsfly/src/components/Hello.vue
+++ pigsfly/src/components/Hello.vue
@@ -7,37 +7,72 @@
     </select>
     <div>
       Weight: <input v-model="weight" >
+      <select v-model="weightUnit">
+         <option>Lbs</option>
+         <option>Kg</option>
+      </select>
     </div>
     <div>
       Length/Height: <input v-model="length" >
+      <select v-model="lengthUnit">
+               <option>Feet</option>
+               <option>Inches</option>
+               <option>Meters</option>
+               <option>Centimetres</option>
+            </select>
     </div>
     <div>
       Wingspan: {{wingspan}}
+    </div>
+    <div>
+
+       <img :src="imageLink" height="150" style="opacity: 0.5" />
     </div>
   </div>
 </template>
 
 <script>
+const AnimalList = [
+  {
+    description: 'pig',
+    weight: 169000,
+    length: 1965
+  }, {
+    description: 'horse',
+    weight: 563500,
+    length: 2500
+  }, {
+    description: 'human',
+    weight: 88677,
+    length: 1770
+  }
+]
+
 export default {
   name: 'hello',
   data () {
     return {
       msg: 'Flying pig wingspan calculator',
-      weight: 100,
-      length: 30,
-      objList: [{
-        description: 'Pig'
-      }, {
-        description: 'Horse'
-      }],
-      selected: {
-        description: 'Pig'
-      }
+      weight: AnimalList[0].weight,
+      length: AnimalList[0].length,
+      objList: AnimalList,
+      selected: AnimalList[0],
+      weightUnit: 'Lbs',
+      lengthUnit: 'Feet'
     }
   },
   computed: {
     wingspan: function () {
       return this.weight * this.length
+    },
+    imageLink: function () {
+      return 'static/' + this.selected.description + '.png'
+    }
+  },
+  watch: {
+    selected: function (val) {
+      this.weight = val.weight
+      this.length = val.length
     }
   }
 }
